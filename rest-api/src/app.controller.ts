eb6dde65d507920a89336132fef6a672faaabd61
Jonny Acevedo <jonnyisdeveloping@gmail.com>
2020-07-23 21:45:24 -0500

    Basic controller and method routes changes

--- rest-api/src/app.controller.ts
+++ rest-api/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('get-message')
   getHello(): string {
     return this.appService.getHello();
   }
