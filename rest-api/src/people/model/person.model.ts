e029aa3755262db95524c2278556fa47e72dc3a0
Jonny Acevedo <jonnyisdeveloping@gmail.com>
2020-07-26 18:33:07 -0500

    PersonModel and save method on service

---
+++ rest-api/src/people/model/person.model.ts
@@ -0,0 +1,8 @@
+export class PersonModel {
+  constructor(
+    private id: number,
+    private name: string,
+    private age: number,
+    private email: string,
+  ) {}
+}
--- rest-api/src/people/service/people/people.service.ts
+++ rest-api/src/people/service/people/people.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { PersonModel } from 'src/people/model/person.model';
 
-@Injectable()
-export class PeopleService {}
+@Controller('people')
+export class PeopleService {
+  private people: Array<PersonModel> = [];
+
+  save(name: string, age: number, email: string): any {
+    const id = Math.random() * 10000 + 1;
+    const person = new PersonModel(id, name, age, email);
+    this.people.push(person);
+    return id;
+  }
+}
