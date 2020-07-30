86b357de02b006dcd09447f97fa1f30ba503021f
Jonny Acevedo <jonnyisdeveloping@gmail.com>
2020-07-29 20:19:29 -0500

    Adding Post to controller

--- rest-api/src/people/controller/people/people.controller.ts
+++ rest-api/src/people/controller/people/people.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { PeopleService } from 'src/people/service/people/people.service';
 
 @Controller('people')
-export class PeopleController {}
+export class PeopleController {
+  constructor(private _peopleService: PeopleService) {}
+
+  @Post()
+  save(
+    @Body() name: string,
+    @Body() age: number,
+    @Body() email: string,
+  ): { ok: boolean; id: number } {
+    const id = this._peopleService.save(name, age, email);
+
+    return { ok: true, id };
+  }
+}
